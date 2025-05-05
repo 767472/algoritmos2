4437412f4dda8387830f857586bf7140287b9ba5
vitor <vitor.amancio@estudante.ifms.edu.br>
2025-05-05 14:42:15 -0400

    modified:   exercicio_1.c 	new file:   exercicio_1.exe

--- exercicio_1.c
+++ exercicio_1.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
- FILE *   
+  int x=0,y=0;
+  int matriz_geral[x][y];  
+  int matriz_resul[x][y];  
+  printf("digite o valor da matriz[x,y]");
+  scanf("%d %d",&x,&y);
+  for(int i=0;i<x;i++)
+  {
+    for(int l=0;l<y;l++)
+    {
+      matriz_geral[i][l]=rand() %255;
+      printf("%d ",matriz_geral[i][l]);
+    }
+    printf("\n");  
+  }
+  for(int i=0;i<5;i++)
+  {
+    for(int l=0;l<5;l++)
+    {
+     
+    }
+    printf("\n");  
+  }
 }
